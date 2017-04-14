22a368a086c38b6ac9a9bed31be40020bf31b499
nayandaKMK <nayanda.haberty@kmklabs.com>
2017-04-14 13:50:32 +0700

    refactor

--- robotf-extension/src/extension.ts
+++ robotf-extension/src/extension.ts
@@ -4,7 +4,7 @@ import * as vscode from 'vscode';
 import {RobotCompletionProvider} from './RobotCompletionProvider';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log("nayanda extension is running");
+	console.log("robot extension is running");
 	context.subscriptions.push(vscode.languages.registerCompletionItemProvider('robot', new RobotCompletionProvider()));
 }
 
