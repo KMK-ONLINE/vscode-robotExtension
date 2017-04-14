bf81538919f9da8cfc5c81093e274fd569ec2844
nayandaKMK <nayanda.haberty@kmklabs.com>
2017-04-14 13:49:06 +0700

    finished keyword suggestion

--- robotf-extension/src/RobotCompletionProvider.ts
+++ robotf-extension/src/RobotCompletionProvider.ts
@@ -1,66 +1,53 @@
 'use strict';
 
 import vscode = require('vscode');
-import {RobotDocUtil} from './RobotDocUtil';
-
+import {KeywordProvider} from './KeywordProvider';
+import {File} from './File';
 export class RobotCompletionProvider implements vscode.CompletionItemProvider{
+	
 	public provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken):Thenable<vscode.CompletionItem[]> | vscode.CompletionItem[]{
-		let thisFileName = RobotDocUtil.fileNameExtractor(document);
+		let thisFile = new File(document.fileName);
+		let thisFileName = thisFile.fileNameWithNoExtension;
 		let line = document.lineAt(position);
-		let keywordSearcherMatch = line.text.match(/^\s+(.+)\.(.*)$/);
-		let firstSearcherMatch = line.text.match(/^\s+(.+)$/);
-		if(keywordSearcherMatch){
-			return this.keywordMatch(document, keywordSearcherMatch[1], keywordSearcherMatch[2]);
+		let firstMatcher = line.text.match(/^\s+(((_|-)*|\w+)+)$/);
+		let secondMatcher = line.text.match(/\s{2,}(((_|-)*|\w+)+)$/)
+		if(firstMatcher){
+			return this.matcher(document, firstMatcher[1]);
 		}
-		else if(firstSearcherMatch){
-			return this.fileMatch(document, firstSearcherMatch[1]);
+		else if(secondMatcher){
+			return this.matcher(document, secondMatcher[1]);
 		}
 		else{
 			return [];
 		}
 	}
 
-	private keywordMatch(document: vscode.TextDocument, fileName:string, keyword:string):vscode.CompletionItem[]{
-		let included = RobotDocUtil.allIncludedResourceSearcher(document);
-		let file:vscode.TextDocument;
-		for(let i = 0; i < included.length; i++){
-			let includedName = RobotDocUtil.fileNameExtractor(included[i]);
-			if(includedName.toUpperCase().includes(fileName.toLocaleUpperCase())){
-				i = included.length;
-				file = included[i];
-			}
-		}
-		let keywords = RobotDocUtil.keywordSearcher(file);
-		let suggestionsString:string[];
-		if(keyword != ""){
-			suggestionsString = RobotDocUtil.sentenceLikelyAnalyzer(keyword, keywords);
-		}
-		else{
-			suggestionsString = keywords;
-		}
-		this.logArray(suggestionsString); //for testing
-		return this.stringArrayToCompletionItems(suggestionsString);
-	}
-
-	private fileMatch(document: vscode.TextDocument, fileName:string):vscode.CompletionItem[]{
-		let included = RobotDocUtil.allIncludedResourceSearcher(document);
-		let includedString = RobotDocUtil.documentsToNames(included);
-		this.logArray(includedString); //for testing
-		let suggestionsString = RobotDocUtil.sentenceLikelyAnalyzer(fileName, includedString);
+	private matcher(document: vscode.TextDocument, fileName:string):vscode.CompletionItem[]{
+		let included = KeywordProvider.allIncludedResourceSearcher(document);
+		let localKeywords = KeywordProvider.vscodeKeywordSearcher(document)
+		let includedKeywords = KeywordProvider.allIncludedKeywordsSearcher(included);
+		let allKeywords = localKeywords.concat(includedKeywords);
+		let suggestionsString = RobotCompletionProvider.sentenceLikelyAnalyzer(fileName, allKeywords);
 		return this.stringArrayToCompletionItems(suggestionsString);
 	}
 
 	private stringArrayToCompletionItems(suggestions:string[]):vscode.CompletionItem[]{
-		let items:vscode.CompletionItem[];
+		let items:vscode.CompletionItem[] = [];
 		for(let i = 0; i < suggestions.length; i++){
 			items.push(new vscode.CompletionItem(suggestions[i]));
 		}
 		return items;
 	}
 
-	private logArray(array:string[]){
-		for(let i = 0; i < array.length; i++){
-			console.log(array[i]);
-		}
-	}
+	public static sentenceLikelyAnalyzer(sentence:string, suggestions:string[]):string[]{
+        let suggestLikely:string[] = [];
+        let search = sentence.toUpperCase();
+        for(let i = 0; i < suggestions.length; i++){
+            let options = suggestions[i].toUpperCase();
+            if(options.includes(search)){
+                suggestLikely.push(suggestions[i]);
+            }
+        }
+        return suggestLikely;
+    }
 }
