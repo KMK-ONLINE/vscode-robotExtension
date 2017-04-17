d0ac37dddba1563085b0b1f6b284e0e6cb086819
nayandaKMK <nayanda.haberty@kmklabs.com>
2017-04-17 10:59:53 +0700

    Update WorkspaceContext.ts

--- src/WorkspaceContext.ts
+++ src/WorkspaceContext.ts
@@ -42,32 +42,30 @@ export class WorkspaceContext {
     public static scanWorkspace() {
         if (WorkspaceContext.asyncReadingCounter == 0) {
             WorkspaceContext.asyncReadingCounter = 1;
-            let scanner = Promise.resolve().then(() => {
-                console.log("scanning all .robot and .txt path");
-                WorkspaceContext.allPath = WorkspaceContext.getAllDirAndDocPath([vscode.workspace.rootPath]);
-                WorkspaceContext.temp = [];
-                WorkspaceContext.asyncReadingCounter += WorkspaceContext.allPath.length;
-                for (let i = 0; i < WorkspaceContext.allPath.length; i++) {
-                    let opener = vscode.workspace.openTextDocument(vscode.Uri.file(WorkspaceContext.allPath[i]));
-                    opener.then((document) => {
-                        WorkspaceContext.temp.push(document);
-                        WorkspaceContext.asyncReadingCounter--;
-                        if (WorkspaceContext.asyncReadingCounter == 1) {
-                            WorkspaceContext.allDoc = WorkspaceContext.temp;
-                            console.log("finished scanning " + WorkspaceContext.allDoc.length + " robot and txt document");
-                            WorkspaceContext.asyncReadingCounter = 0;
-                        }
-                    }, (reason) => {
-                        console.log(reason);
-                        WorkspaceContext.asyncReadingCounter--;
-                        if (WorkspaceContext.asyncReadingCounter == 1) {
-                            WorkspaceContext.allDoc = WorkspaceContext.temp;
-                            console.log("finished scanning " + WorkspaceContext.allDoc.length + " robot and txt document");
-                            WorkspaceContext.asyncReadingCounter = 0;
-                        }
-                    });
-                }
-            });
+            console.log("scanning all .robot and .txt path");
+            WorkspaceContext.allPath = WorkspaceContext.getAllDirAndDocPath([vscode.workspace.rootPath]);
+            WorkspaceContext.temp = [];
+            WorkspaceContext.asyncReadingCounter += WorkspaceContext.allPath.length;
+            for (let i = 0; i < WorkspaceContext.allPath.length; i++) {
+                let opener = vscode.workspace.openTextDocument(vscode.Uri.file(WorkspaceContext.allPath[i]));
+                opener.then((document) => {
+                    WorkspaceContext.temp.push(document);
+                    WorkspaceContext.asyncReadingCounter--;
+                    if (WorkspaceContext.asyncReadingCounter == 1) {
+                        WorkspaceContext.allDoc = WorkspaceContext.temp;
+                        console.log("finished scanning " + WorkspaceContext.allDoc.length + " robot and txt document");
+                        WorkspaceContext.asyncReadingCounter = 0;
+                    }
+                }, (reason) => {
+                    console.log(reason);
+                    WorkspaceContext.asyncReadingCounter--;
+                    if (WorkspaceContext.asyncReadingCounter == 1) {
+                        WorkspaceContext.allDoc = WorkspaceContext.temp;
+                        console.log("finished scanning " + WorkspaceContext.allDoc.length + " robot and txt document");
+                        WorkspaceContext.asyncReadingCounter = 0;
+                    }
+                });
+            }
         }
     }
 
@@ -75,9 +73,9 @@ export class WorkspaceContext {
         let allFiles: string[] = [];
         for (let i = 0; i < paths.length; i++) {
             if (fs.lstatSync(paths[i]).isDirectory()) {
-                let directory = fs.readdirSync(paths[i] + "\\");
+                let directory = fs.readdirSync(paths[i] + "\/");
                 for (let j = 0; j < directory.length; j++) {
-                    directory[j] = paths[i] + "\\" + directory[j];
+                    directory[j] = paths[i] + "\/" + directory[j];
                 }
                 allFiles = allFiles.concat(WorkspaceContext.getAllDirAndDocPath(directory))
             }
