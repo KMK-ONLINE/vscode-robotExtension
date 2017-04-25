1dcb421423dcd84d9b380b7e56c9dca3dcdba1ad
Nayanda Haberty <naysone@yahoo.com>
2017-04-25 16:41:24 +0700

    initialize file context to improve performance

---
+++ src/FileContext.ts
@@ -0,0 +1,15 @@
+'use strict'
+
+import { Keyword } from './model/Keyword';
+import { Variable } from './model/Variable';
+
+export class FileContext{
+    
+    private static keywords: Keyword[];
+    private static variable: Variable[];
+
+    public static scanFiles(){
+
+    }
+
+}
